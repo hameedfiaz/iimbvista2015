505317d0e11027cddd19ea04399b766760d6234c
hameedfiaz <rmahameed005@gmail.com>
2015-07-29 06:25:32 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iimbvista2015::Application.config.secret_token = 'b607ff4a924e5d6124315b14bcca3bd61f000f6c2dc4a276f664e8f197ea2ee78391f221659e5d49691f1981fccf5c1ea9793352d6e2f772d0c4901af2a02d33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Iimbvista2015::Application.config.session_store :cookie_store, key: '_iimbvista2015_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Iimbvista2015::Application.config.session_store :active_record_store
