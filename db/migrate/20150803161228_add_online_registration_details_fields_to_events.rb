97237553bb2748c4d3f2a447aa69a6abc282db65
hameedfiaz <rmahameed005@gmail.com>
2015-08-05 10:41:28 +0530

    For rollback

--- db/migrate/20150803161228_add_online_registration_details_fields_to_events.rb
+++ db/migrate/20150803161228_add_online_registration_details_fields_to_events.rb
@@ -1,8 +1,6 @@
 class AddOnlineRegistrationDetailsFieldsToEvents < ActiveRecord::Migration
   def change
-  	add_column :events, :min_per_team, :integer, :default => 0
-  	add_column :events, :max_per_team, :integer, :default => 0
-  	add_column :events, :deadline, :date
+  	add_column :events, :per_team, :integer, :default => 0
   	add_column :events, :permitted_registrations, :integer, :default => 0
   	add_column :events, :registration_fee, :integer, :default => 0
   	add_column :events, :flagship, :boolean, :default => false
