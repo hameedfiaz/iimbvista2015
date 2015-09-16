a476355c10b4b188adf6e64d802a1a1944c99ca6
hameedfiaz <rmahameed005@gmail.com>
2015-09-17 00:34:22 +0530

    api changes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,23 +1,21 @@
 class ApiController < ApplicationController
 
   def auth_user
-  	if request.post?
-	  	email=params[:email]
-	  	password=params[:password]
-	  	begin
-	  		user=User.find_by_email(email)
-	  		if user
-	  			res = user.valid_password?(password)
-	  		end
-	  	rescue
-	  		render :text => "Oops ! Something went wrong", :status => 503 and return
-	  	end
-	  	if res
-	  		render :text => "Authenticated Successfully" and return
-	  	else
-	  		render :text => "Authentication Failed", :status => 500 and return
-	  	end
-	end
+  	email=params[:email]
+  	password=params[:password]
+  	begin
+  		user=User.find_by_email(email)
+  		if user
+  			res = user.valid_password?(password)
+  		end
+  	rescue
+  		render :text => "Oops ! Something went wrong", :status => 503 and return
+  	end
+  	if res
+  		render :text => "Sucess" and return
+  	else
+  		render :text => "Authentication Failed", :status => 500 and return
+  	end
   end
 
 end
