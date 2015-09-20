eac8d403f614bb37c1edddd3584da4a3ca62447b
hameedfiaz <rmahameed005@gmail.com>
2015-09-21 04:21:13 +0530

    mailer change

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -36,7 +36,7 @@ class MyMailer < Devise::Mailer
   def launch_mailer_second(email)
     # attachments.inline['footer.png'] = File.read("#{Rails.root}/app/assets/images/img/mail_footer.png")
     attachments.inline['header.png'] = File.read("#{Rails.root}/app/assets/images/img/mail_header.png")
-    mail(:from => "vista@iimb.ernet.in", :to => email, :subject => "IIM Bangalore's VISTA: GoViral Deadline update!")
+    mail(:from => "vista@iimb.ernet.in", :to => email, :subject => "IIM Bangalore's Vista: Presenting Keynote Speakers: Mr. Rakeysh Omprakash Mehra and Mr.Nitin Gadkari!")
   end
 
   def launch_mailer_third(email)
