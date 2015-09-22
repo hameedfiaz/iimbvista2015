f72721d777c90d88b2dfdf1a98c9e3edc1861e6b
hameedfiaz <rmahameed005@gmail.com>
2015-09-22 15:58:55 +0530

    order schedule

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   end
 
   def schedule
-  	@schedule_items=ScheduleItem.all
+  	@schedule_items=ScheduleItem.all(:order => 'event_start, event_end')
   	@days=@schedule_items.collect(&:event_start).collect(&:to_date).uniq
   end
 end
