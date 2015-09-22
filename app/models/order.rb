8aa36c93d6e9fa2d6a3cf168994e638b333dc135
hameedfiaz <rmahameed005@gmail.com>
2015-09-23 03:03:26 +0530

    stupid

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
 
-	attr_accessible :user_id, :total_amount, :created_at, :updated_at
+	attr_accessible :user_id, :total_amount, :created_at, :updated_at, :order_id, :cart_amount
 
 	belongs_to :user
 	has_many :order_items
